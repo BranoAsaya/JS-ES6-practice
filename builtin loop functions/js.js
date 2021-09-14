0a60d88df4ad1ed2cc56e63955d0d7a081c37fe7
BranoAsaya <87427429+BranoAsaya@users.noreply.github.com>
2021-09-14 14:08:03 +0300

    update the README.md with link to contributing guide

---
+++ builtin loop functions/js.js
@@ -0,0 +1,125 @@
+// const arrStr = ["yo","ok","zo"];
+
+// arrStr.forEach(e =>console.log(e));
+
+// function getStr(str) {
+//     console.log(str);
+//   }
+  
+//   arrStr.forEach(getStr);
+
+// function getStr(str, i) {
+//     console.log(`${str} index  ${i}`);
+//   }
+  
+//   arrStr.forEach(getStr);
+
+// const arrNum = [5,2,3,1];
+ 
+// function getNum(num,i){
+//     console.log(`${num} index ${i}`);
+// }
+// arrNum.forEach(getNum)
+
+// const arrNumX=[2,7,5,6,4];
+
+// function getNumDual(num,i){
+//     console.log(`${num*7} index ${i}`);
+// }
+// arrNumX.forEach(getNumDual)
+
+// const arrNumE=[2,7,5,6,4];
+
+// function getEvenNum(num){
+//     if(num%2==0){
+//         console.log(`${num} yes`);
+//     }
+//     else{console.log("no");}
+// }
+// arrNumE.forEach(getEvenNum)
+
+// const arrObj=[
+//     {name:"yo",age:55},
+//     {name:"lo",age:78},
+//     {name:"so",age:14}
+    
+// ];
+
+// for (const obj of arrObj) {
+//     console.log(obj);
+// }
+
+// const arrNumS=[5,7,4,6,1];
+// let sum=0;
+// for (const it of arrNumS) {
+//     sum+=it;
+// }
+// console.log(sum);
+
+// const obj={name:"lo",age:55};
+// for (const key in obj) {
+//   console.log(key);
+//   console.log(obj[key]);
+// }
+//עבודת בית:
+//!1.a
+// const arrArticle=[
+//     {title:"yes",author:"mike",price:20},
+//     {title:"no",author:"jack",price:10},
+//     {title:"ok",author:"ted",price:15}
+// ];
+
+// function getArticle(art){
+//     console.log(art);
+
+// }
+// arrArticle.forEach(getArticle)
+
+//!.b
+
+// for (const it of arrArticle) {
+//     console.log(it);
+// }
+
+//!2.a
+
+// const arrObjI = [
+//     {name:"lo",age:55},
+//     {name:"yo",age:47},
+//     {name:"so",age:36}
+// ];
+
+// for (const key in arrObjI) {
+
+//    console.log(`${arrObjI[key].name}${arrObjI[key].age}`);
+// }
+
+//!3.a
+// const arrBooks=[
+//     {title:"yes",author:"mike",price:20},
+//     {title:"no",author:"jack",price:10},
+//     {title:"ok",author:"ted",price:15},
+//     {title:"yo",author:"pit",price:15}
+// ]
+// for (const book of arrBooks) {
+//     console.log(book);
+// }
+
+//!3.i
+
+// for (const key in arrBooks) {
+//   console.log(arrBooks[key].author);
+// }
+
+//!4.a
+
+// const arrSNum=[5,7,1,3,6]
+// let sum=0;
+// for (const it of arrSNum) {
+//     sum+=it;
+// }
+// console.log(sum);
+
+
+
+
